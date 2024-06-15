7c0056a487f9af34dee6e451eb2c2303392912a2
v1a0 <contact@v1a0.dev>
2024-06-15 08:58:34 +0700

    [3] Add SoftDeleteModel

---
+++ django_utk/db/models/softdelete/__init__.py
@@ -0,0 +1,15 @@
+from django_utk.db.models.softdelete.querysets import *
+from django_utk.db.models.softdelete.querysets import __all__ as querysets_all
+from django_utk.db.models.softdelete.managers import *
+from django_utk.db.models.softdelete.managers import __all__ as managers_all
+from django_utk.db.models.softdelete.models import *
+from django_utk.db.models.softdelete.models import __all__ as mixins_all
+from django_utk.db.models.softdelete.models import *
+from django_utk.db.models.softdelete.models import __all__ as models_all
+
+__all__ = [
+    *querysets_all,
+    *managers_all,
+    *mixins_all,
+    *models_all,
+]
---
+++ django_utk/db/models/softdelete/querysets.py
@@ -0,0 +1,25 @@
+from typing import Self
+
+from django.db import models
+from django.utils import timezone
+
+
+__all__ = [
+    "SoftDeleteQuerySet",
+]
+
+
+class SoftDeleteQuerySet(models.QuerySet):
+    def delete(self):
+        self.update(deleted_at=timezone.now())
+        return True, self.count()
+
+    def restore(self):
+        self.update(deleted_at=None)
+        return True, self.count()
+
+    def hard_delete(self):
+        return super(SoftDeleteQuerySet, self).delete()
+
+    def filter(self, *args, **kwargs) -> Self:
+        return super(SoftDeleteQuerySet, self).filter(*args, **kwargs)
