e60dd446853a9e49ffd0ef972921f127e62fad2c
v1a0 <contact@v1a0.dev>
2024-06-17 12:05:09 +0700

    [1] Add humanreadable names to TimeStampedModel fields

--- django_utk/db/models/temestamped/models.py
+++ django_utk/db/models/temestamped/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from django_utk.db.fields import CreatedAtField, UpdatedAtField
+from django.utils.translation import gettext_lazy as _
 
 
 __all__ = [
@@ -10,8 +11,8 @@ __all__ = [
 
 class TimeStampedModel(models.Model):
 
-    created_at = CreatedAtField()
-    updated_at = UpdatedAtField()
+    created_at = CreatedAtField(_("Created at"))
+    updated_at = UpdatedAtField(_("Updated at"))
 
     class Meta:
         abstract = True
