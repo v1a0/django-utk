65328497801cecba066fd620b3f22b8e93de5936
v1a0 <contact@v1a0.dev>
2024-06-17 12:02:35 +0700

    [*] Remove useless imports

--- django_utk/db/models/softdelete/__init__.py
+++ django_utk/db/models/softdelete/__init__.py
@@ -3,13 +3,10 @@ from django_utk.db.models.softdelete.querysets import __all__ as querysets_all
 from django_utk.db.models.softdelete.managers import *
 from django_utk.db.models.softdelete.managers import __all__ as managers_all
 from django_utk.db.models.softdelete.models import *
-from django_utk.db.models.softdelete.models import __all__ as mixins_all
-from django_utk.db.models.softdelete.models import *
 from django_utk.db.models.softdelete.models import __all__ as models_all
 
 __all__ = [
     *querysets_all,
     *managers_all,
-    *mixins_all,
     *models_all,
 ]
