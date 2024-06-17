94888820993672986857e70edd062e7fb7225864
v1a0 <contact@v1a0.dev>
2024-06-17 12:03:46 +0700

    [1] Bugfix super process

--- django_utk/db/fields/updated_at.py
+++ django_utk/db/fields/updated_at.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.utils.translation import gettext_lazy as _
 
 
 __all__ = [
@@ -15,7 +14,7 @@ class UpdatedAtField(models.DateTimeField):
 
     def __init__(self, *args, **kwargs):
         kwargs.update(self._required_kwargs)
-        super().__init__(self, *args, **kwargs)
+        models.DateTimeField.__init__(self, *args, **kwargs)
 
     def deconstruct(self):
         name, path, args, kwargs = super().deconstruct()
