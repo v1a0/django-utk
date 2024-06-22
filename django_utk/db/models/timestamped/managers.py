a3aa3d36036755956bdfe15ec9ac318fb7ec27c9
v1a0 <contact@v1a0.dev>
2024-06-22 18:24:33 +0700

    [*] Add TimeStampedManager

---
+++ django_utk/db/models/timestamped/managers.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+from common.abc import ABCManager
+from django_utk.db.models.timestamped.querysets import TimeStampedQuerySet
+
+
+class BaseTimeStampedManager(ABCManager):
+    QuerySetClass = TimeStampedQuerySet
+
+
+class TimeStampedManager(BaseTimeStampedManager, models.Manager):
+    pass
+
+
