6d738940b75c79c63586165f7b841fdd20596ba4
v1a0 <contact@v1a0.dev>
2024-06-24 22:05:47 +0700

    [9] Faker, add hashsum-s generators, __init__.py

--- django_utk/tests/faker/__init__.py
+++ django_utk/tests/faker/__init__.py
@@ -1,5 +1,7 @@
 from django_utk.tests.faker.choices import *
 from django_utk.tests.faker.choices import __all__ as choices_all
+from django_utk.tests.faker.hashes import *
+from django_utk.tests.faker.hashes import __all__ as hashes_all
 from django_utk.tests.faker.numeric import *
 from django_utk.tests.faker.numeric import __all__ as numeral_all
 from django_utk.tests.faker.sequences import *
@@ -12,4 +14,5 @@ __all__ = [
     *numeral_all,
     *sequences_all,
     *choices_all,
+    *hashes_all,
 ]
