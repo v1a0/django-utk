442aac6d203ead4161aa4ab8ed217940440b888a
v1a0 <contact@v1a0.dev>
2024-06-25 11:45:14 +0700

    [9] refactor tests

--- tests/tests__tests_faker__randstring.py
+++ tests/tests__tests_faker__randstring.py
@@ -1,16 +1,13 @@
-import random
 import string
 from unittest import TestCase
 
 from django_utk.tests import faker
 from django_utk.tests.run_n_times import run_100_times
 
+rand_int = faker.RandInt(1, 42)
+
 
 class RandStringTestCase(TestCase):
-
-    A = 42
-    ALPHABET = string.printable
-
     @run_100_times
     def assertResultValid(self, string_factory: callable, length: int, alphabet: set):
         result = string_factory()
@@ -25,11 +22,9 @@ class RandStringTestCase(TestCase):
             string_fct, faker.RandString.LENGTH, faker.RandString.ALPHABET
         )
 
+    @run_100_times
     def test__alphabet(self):
-        for alphabet_len in range(1, self.A):
-            alphabet = random.choices(self.ALPHABET, k=alphabet_len)
-
-            for string_len in range(1, self.A):
-                string_fct = faker.RandString(string_len, alphabet=alphabet)
-
-                self.assertResultValid(string_fct, string_len, set(alphabet))
+        string_len = rand_int()
+        alphabet = string.printable[: min(len(string.printable), rand_int())]
+        string_fct = faker.RandString(string_len, alphabet=alphabet)
+        self.assertResultValid(string_fct, string_len, set(alphabet))
