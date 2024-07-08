550e96c2e01d8706005916f140c25b796bc56e58
v1a0 <contact@v1a0.dev>
2024-07-08 14:53:52 +0700

    [*] Fix imports

--- django_utk/db/models/dbview/managers.py
+++ django_utk/db/models/dbview/managers.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from django_utk.db.models.dbview import DBViewQuerySet
+from django_utk.db.models.dbview.querysets import DBViewQuerySet
 
 __all__ = [
     "DBViewManager",
